656713742b0fd58365ed684b36add059eb20620e
vicky-fang-xuanxuan <158122835+vicky-fang-xuanxuan@users.noreply.github.com>
2025-02-26 17:16:09 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -35,27 +35,3 @@ module bitwise_majority (
   assign C = (A & B) | (A ^ B); // Bitwise majority function
 
 endmodule
-
-module tb_bitwise_majority;
-    reg [7:0] A, B;
-    wire [7:0] C;
-
-    bitwise_majority uut (
-        .A(A),
-        .B(B),
-        .C(C)
-    );
-
-    initial begin
-        $dumpfile("tb.vcd");
-        $dumpvars(0, tb_bitwise_majority);
-
-        // Test Cases
-        A = 8'b11001100; B = 8'b10101010; #10;
-        A = 8'b00000001; B = 8'b00000000; #10;
-        A = 8'b11111111; B = 8'b00000000; #10;
-        A = 8'b01010101; B = 8'b10101010; #10;
-
-        $finish;
-    end
-endmodule
